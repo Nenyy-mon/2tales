62411091373c950f7157064b603258f67ba3e649
Nenyy-mon <shone2699@hotmail.rs>
2024-05-23 20:41:51 +0200

    proxy changes6

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://virmigo.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://2tales-8s25.vercel.app/");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     res.setHeader(
